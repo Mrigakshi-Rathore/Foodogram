c6291b70c3382b5710c5fcf120f25b0782221874
Priyanka Kumari <priyankakumari_ee21b9_01@dtu.ac.in>
2025-11-18 04:21:23 +0530

    Enhancement: Footer Layout and aligment

--- footer.php
+++ footer.php
@@ -1,18 +1,17 @@
-    <footer class="footer text-white bg-black pt-4 pb-2 mt-5">
-        <div class="container text-center text-md-start">
-            <div class="row">
+    <footer class="footer text-white bg-black">
+        <div class="container">
+            <div class="row align-items-start">
                 <!-- Logo and Description -->
-                <div class="col-md-4 mb-3">
-                    <h5 class="glowbox" class="fw-bold" style="color: rgb(255, 200, 0); font-size: 30px;">🍽️ Foodogram</h5>
+                <div class="col-md-4">
+                    <h5 style="color: #ffc800;">🍽️ Foodogram</h5>
                     <p>
-                        Your go-to destination for fresh, fast & delicious food delivered
-                        at your door.
+                        Your go-to destination for fresh, fast & delicious food delivered at your door.
                     </p>
                 </div>
 
                 <!-- Quick Links -->
-                <div class="col-md-4 mb-3">
-                    <h6 class="fw-bold" style="color: yellow;">Quick Links</h6>
+                <div class="col-md-3">
+                    <h6 style="color: #ffc800;">Quick Links</h6>
                     <ul class="list-unstyled">
                         <li>
                             <a href="about.php" class="text-white text-decoration-none">About Us</a>
@@ -30,16 +29,18 @@
                 </div>
 
                 <!-- Social Media -->
-                <div class="col-md-4 mb-3">
-                    <h6 style="color: yellow;" class="fw-bold">Follow Us</h6>
-                    <a href="#" class="text-white me-3"><i class="fab fa-facebook fa-lg"></i></a>
-                    <a href="#" class="text-white me-3"><i class="fab fa-instagram fa-lg"></i></a>
-                    <a href="#" class="text-white me-3"><i class="fab fa-twitter fa-lg"></i></a>
-                    <a href="#" class="text-white"><i class="fab fa-youtube fa-lg"></i></a>
+                <div class="col-md-3">
+                    <h6 style="color: #ffc800;">Follow Us</h6>
+                    <div class="social-links">
+                        <a href="#" class="text-white"><i class="fab fa-facebook"></i></a>
+                        <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="text-white"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="text-white"><i class="fab fa-youtube"></i></a>
+                    </div>
                 </div>
             </div>
             <hr class="border-secondary" />
-            <div style="color: yellow;" class="text-center small">
+            <div class="copyright text-center">
                 &copy; 2025 Foodogram. All rights reserved.
             </div>
         </div>
